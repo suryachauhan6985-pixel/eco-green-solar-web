6c8dab206d4eefdb9b3794c33c81ca52f1c4c5d9
Sumit Chauhan <suryachauhan6985@gmail.com>
2026-08-25 11:58:43 +0530

    Add neon tracer glow button animation to logout with rounded corners

--- js/app.js
+++ js/app.js
@@ -5620,10 +5620,8 @@ window.attachColumnFilters = function (table) {
       <div class="profile-menu-divider"></div>
       <button type="button" class="profile-menu-item" id="profileSettings"><i class="fa-solid fa-gear"></i> System Settings</button>
       <button type="button" class="profile-menu-item" id="profileInstallApp"><i class="fa-solid fa-download"></i> Install ERP App (iOS / Android / PC)</button>
-      <button type="button" class="profile-menu-item" id="profileLoginActivity"><i class="fa-solid fa-mobile-screen-button"></i> Login activity</button>
       <button type="button" class="uiverse-logout-btn" id="profileLogout" title="Log out of ERP">
-        <div class="sign"><svg viewBox="0 0 512 512"><path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path></svg></div>
-        <div class="text">Log out</div>
+        <span><i class="fa-solid fa-right-from-bracket"></i> LOG OUT</span><i class="neon-wire"></i>
       </button>`;
     document.body.appendChild(menu);
 
